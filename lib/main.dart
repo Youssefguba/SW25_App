d4703a6e29cbdbed7c8d0d75758c2a8270bc3e90
Youssefguba <Youssef.guba@abwaab.me>
2022-10-16 21:41:44 +0200

    some updates.

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/cupertino.dart';
 
 void main() {
   runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
     home: LoginPage(),
   ));
 }
@@ -14,8 +15,7 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Container(
-          width: MediaQuery.of(context).size.width,
+        child: SingleChildScrollView(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.center,
@@ -44,15 +44,108 @@ class LoginPage extends StatelessWidget {
                 ),
               ),
               SizedBox(height: 20),
-              TextFormField(
-                decoration: InputDecoration(
-                  hintText: "Enter Your Email",
-                  prefixIcon: Icon(Icons.email)
-                  // suffixIcon: Icon(Icons.)
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 12, vertical: 12),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    hintText: "Enter Your Email",
+                    prefixIcon: Icon(Icons.email),
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.red,
+                        width: 12,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 12, vertical: 12),
+                child: TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    hintText: "Enter Your Password",
+                    prefixIcon: Icon(Icons.lock_outline_rounded),
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.red,
+                        width: 12,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                width: 190,
+                child: ElevatedButton(
+                  onPressed: () {
+                    print('Hello World!');
+                  },
+                  child: Text('Sign In'),
+                  style: ElevatedButton.styleFrom(
+                    primary: Color(0xff40bfff),
+                  ),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                      height: 1,
+                      width: MediaQuery.of(context).size.width * 0.25,
+                      color: Colors.grey),
+                  SizedBox(width: 10),
+                  Text('OR', style: TextStyle(color: Colors.grey)),
+                  SizedBox(width: 10),
+                  Container(height: 1, width: 100, color: Colors.grey),
+                ],
+              ),
+              Container(
+                width: 190,
+                child: TextButton.icon(
+                  onPressed: () {
+                    print('Hello World!');
+                  },
+                  label: Text('Login with Google'),
+                  icon: Icon(Icons.g_mobiledata_rounded),
+                ),
+              ),
+              Container(
+                width: 190,
+                child: TextButton.icon(
+                  onPressed: () {
+                    Navigator.of(context).pushAndRemoveUntil(
+                        MaterialPageRoute(builder: (context) {
+                          return RegisterPage();
+                        }), (route) => false);
+                  },
+                  label: Text('SignUp'),
+                  icon: Icon(Icons.email),
                 ),
               ),
             ],
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class RegisterPage extends StatelessWidget {
+  const RegisterPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.of(context).pushAndRemoveUntil(
+                MaterialPageRoute(builder: (context) {
+              return LoginPage();
+            }), (route) => false);
+          },
+          child: Text('Login'),
         ),
       ),
     );
