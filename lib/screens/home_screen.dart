90233a940f375f6240bf40350cb0220f371f9b08
Youssefguba <Youssef.guba@abwaab.me>
2022-10-26 22:14:49 +0200

    some updates.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,137 +1,74 @@
 import 'package:ecommerce_app_sw25/models/category_model.dart';
+import 'package:ecommerce_app_sw25/screens/account_screen.dart';
+import 'package:ecommerce_app_sw25/screens/cart_screen.dart';
+import 'package:ecommerce_app_sw25/screens/offers_screen.dart';
+import 'package:ecommerce_app_sw25/screens/search_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 
-class HomeScreen extends StatelessWidget {
-  List<String> listOfImages = [
-    'assets/images/promotion_image.png',
-    'assets/images/promotion_image.png',
-    'assets/images/promotion_image.png',
-    'assets/images/promotion_image.png',
+import 'main_screen.dart';
+
+/// Widgets divided into two types : Stateless / Stateful
+
+/// (1) Create Bottom Navigation ( DONE )
+/// (2) Create Screens [ Search, Cart, Offers, Account ] ( DONE )
+/// (3) Create navigationIndex --> currentIndex AND onTap AND listOfScreens
+/// (4) put the listOfScreens in body by Index.
+class HomeScreen extends StatefulWidget {
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+
+  int navigationIndex = 0;
+
+  List<Widget> screenList = [
+    MainScreen(),    // 0
+    SearchScreen(),  // 1
+    CartScreen(),    // 2
+    OffersScreen(),  // 3
+    AccountScreen(), // 4
   ];
 
-  List<CategoryModel> listOfCategories = [
-    CategoryModel(image: "assets/images/shirt.png", title: "Man Cloths", inStock: true),
-    CategoryModel(image: "assets/images/shirt.png", title: "Man Cloths", inStock: true),
-    CategoryModel(image: "assets/images/shirt.png", title: "Man Cloths", inStock: true),
-    CategoryModel(image: "assets/images/shirt.png", title: "Man Cloths", inStock: true),
-    CategoryModel(image: "assets/images/shirt.png", title: "Man Cloths", inStock: true),
-    CategoryModel(image: "assets/images/shirt.png", title: "Man Cloths", inStock: true),
-    CategoryModel(image: "assets/images/shirt.png", title: "Man Cloths", inStock: true),
-    CategoryModel(image: "assets/images/shirt.png", title: "Man Cloths", inStock: true),
-    CategoryModel(image: "assets/images/shirt.png", title: "Man Cloths", inStock: true),
-    CategoryModel(image: "assets/images/shirt.png", title: "Man Cloths", inStock: true),
-    CategoryModel(image: "assets/images/shirt.png", title: "Man Cloths", inStock: true),
-    CategoryModel(image: "assets/images/shirt.png", title: "Man Cloths", inStock: true),
-  ];
+  @override
+  void initState() {
+    print('Fetch Slider Photos !!.');
+    super.initState();
+  }
+
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    print('Context is Here!');
+  }
+
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: ListView(
-          children: [
-            // Search Bar - Wishlist Item - Notification
-            Padding(
-              padding: const EdgeInsets.all(12.0),
-              child: Row(
-                children: [
-                  Container(
-                    width: MediaQuery.of(context).size.width * 0.7,
-                    child: TextFormField(
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        prefixIcon: Icon(Icons.search),
-                        hintText: "Search Product",
-                      ),
-                    ),
-                  ),
-                  SizedBox(width: 12),
-                  Icon(Icons.favorite_border_outlined),
-                  SizedBox(width: 12),
-                  Icon(Icons.notifications_none),
-                ],
-              ),
-            ),
-
-            // Slider
-            CarouselSlider(
-              options: CarouselOptions(
-                height: 200.0,
-                autoPlay: true,
-              ),
-              items: listOfImages.map((i) {
-                return Builder(
-                  builder: (BuildContext context) {
-                    return Container(
-                      width: MediaQuery.of(context).size.width,
-                      margin: EdgeInsets.symmetric(horizontal: 5.0),
-                      child: Image.asset(i),
-                    );
-                  },
-                );
-              }).toList(),
-            ),
-
-            // Title of Category
-            Row(),
-
-            SizedBox(height: 20),
-
-            // List of Categories
-            Container(
-              height: 100,
-              child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemCount: listOfCategories.length,
-                itemBuilder: (context, index) {
-                  return Container(
-                    padding: EdgeInsets.symmetric(horizontal: 8),
-                    child: Column(
-                      children: [
-                        CircleAvatar(
-                          child: Image.asset(listOfCategories[index].image),
-                          backgroundColor: listOfCategories[index].inStock
-                              ? Colors.orange
-                              : Colors.red,
-                        ),
-                        Text(listOfCategories[index].title),
-                      ],
-                    ),
-                  );
-                },
-              ),
-            ),
-
-            GridView.builder(
-              itemCount: listOfCategories.length,
-              shrinkWrap: true,
-              itemBuilder: (context, index) {
-                return Container(
-                  padding: EdgeInsets.symmetric(horizontal: 8),
-                  child: Column(
-                    children: [
-                      CircleAvatar(
-                        child: Image.asset(listOfCategories[index].image),
-                        backgroundColor: listOfCategories[index].inStock
-                            ? Colors.orange
-                            : Colors.red,
-                      ),
-                      Text(listOfCategories[index].title),
-                    ],
-                  ),
-                );
-              },
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 3,
-                mainAxisSpacing: 80,
-                crossAxisSpacing: 30,
-                childAspectRatio: 1,
-              ),
-            ),
-          ],
-        ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        onTap: (int index) {
+          // callback function
+          setState(() {
+            navigationIndex = index;
+          }); // anonymous function
+        },
+        currentIndex: navigationIndex, // 3
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"), // 0
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: "Search"), // 1
+          BottomNavigationBarItem(icon: Icon(Icons.shopping_cart_outlined), label: "Cart"), // 2
+          BottomNavigationBarItem(icon: Icon(Icons.local_offer_outlined), label: "Offer"), // 3
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: "Account"), // 4
+        ],
       ),
+      body: screenList[navigationIndex],
     );
   }
 }
+
+
---
+++ lib/screens/main_screen.dart
@@ -0,0 +1,162 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:ecommerce_app_sw25/models/category_model.dart';
+import 'package:flutter/material.dart';
+
+import 'favourite_screen.dart';
+
+class MainScreen extends StatelessWidget {
+
+  List<String> listOfImages = [
+    'assets/images/promotion_image.png',
+    'assets/images/promotion_image.png',
+    'assets/images/promotion_image.png',
+    'assets/images/promotion_image.png',
+  ];
+
+  List<CategoryModel> listOfCategories = [
+    CategoryModel(
+        image: "assets/images/shirt.png", title: "Man Cloths", inStock: true),
+    CategoryModel(
+        image: "assets/images/shirt.png", title: "Man Cloths", inStock: true),
+    CategoryModel(
+        image: "assets/images/shirt.png", title: "Man Cloths", inStock: true),
+    CategoryModel(
+        image: "assets/images/shirt.png", title: "Man Cloths", inStock: true),
+    CategoryModel(
+        image: "assets/images/shirt.png", title: "Man Cloths", inStock: true),
+    CategoryModel(
+        image: "assets/images/shirt.png", title: "Man Cloths", inStock: true),
+    CategoryModel(
+        image: "assets/images/shirt.png", title: "Man Cloths", inStock: true),
+    CategoryModel(
+        image: "assets/images/shirt.png", title: "Man Cloths", inStock: true),
+    CategoryModel(
+        image: "assets/images/shirt.png", title: "Man Cloths", inStock: true),
+    CategoryModel(
+        image: "assets/images/shirt.png", title: "Man Cloths", inStock: true),
+    CategoryModel(
+        image: "assets/images/shirt.png", title: "Man Cloths", inStock: true),
+    CategoryModel(
+        image: "assets/images/shirt.png", title: "Man Cloths", inStock: true),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: ListView(
+        children: [
+          // Search Bar - Wishlist Item - Notification
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Row(
+              children: [
+                Container(
+                  width: MediaQuery.of(context).size.width * 0.7,
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      prefixIcon: Icon(Icons.search),
+                      hintText: "Search Product",
+                    ),
+                  ),
+                ),
+                SizedBox(width: 12),
+                IconButton(
+                  icon: Icon(Icons.favorite_border_outlined),
+                  onPressed: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) {
+                          return FavouriteScreen();
+                        },
+                      ),
+                    );
+                  },
+                ),
+                SizedBox(width: 12),
+                Icon(Icons.notifications_none),
+              ],
+            ),
+          ),
+
+          // Slider
+          CarouselSlider(
+            options: CarouselOptions(
+              height: 200.0,
+              autoPlay: true,
+            ),
+            items: listOfImages.map((i) {
+              return Builder(
+                builder: (BuildContext context) {
+                  return Container(
+                    width: MediaQuery.of(context).size.width,
+                    margin: EdgeInsets.symmetric(horizontal: 5.0),
+                    child: Image.asset(i),
+                  );
+                },
+              );
+            }).toList(),
+          ),
+
+          // Title of Category
+          Row(),
+
+          SizedBox(height: 20),
+
+          // List of Categories
+          Container(
+            height: 100,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: listOfCategories.length,
+              itemBuilder: (context, index) {
+                return Container(
+                  padding: EdgeInsets.symmetric(horizontal: 8),
+                  child: Column(
+                    children: [
+                      CircleAvatar(
+                        child: Image.asset(listOfCategories[index].image),
+                        backgroundColor: listOfCategories[index].inStock
+                            ? Colors.orange
+                            : Colors.red,
+                      ),
+                      Text(listOfCategories[index].title),
+                    ],
+                  ),
+                );
+              },
+            ),
+          ),
+
+          GridView.builder(
+            itemCount: listOfCategories.length,
+            shrinkWrap: true,
+            physics: NeverScrollableScrollPhysics(),
+            itemBuilder: (context, index) {
+              return Container(
+                padding: EdgeInsets.symmetric(horizontal: 8),
+                child: Column(
+                  children: [
+                    CircleAvatar(
+                      child: Image.asset(listOfCategories[index].image),
+                      backgroundColor: listOfCategories[index].inStock
+                          ? Colors.orange
+                          : Colors.red,
+                    ),
+                    Text(listOfCategories[index].title),
+                  ],
+                ),
+              );
+            },
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3,
+              mainAxisSpacing: 80,
+              crossAxisSpacing: 30,
+              childAspectRatio: 1,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
